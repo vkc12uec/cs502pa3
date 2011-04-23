5317aadf910546cebf6f57126b20e8a05cf8d891
vivekchaurasiya@gmail.com <vivekchaurasiya@gmail.com@856bd569-48ee-681a-51fe-52a91ea4caff>
2011-04-23 20:45:38 +0000

    Adding some code for Build phase while looking at George's paper. Have a look. Some correctiion is needed in buildphase.

--- trunk/p3/src/RegAlloc/Liveness.java
+++ trunk/p3/src/RegAlloc/Liveness.java
@@ -16,42 +16,38 @@ public class Liveness extends InterferenceGraph {
         return moves;
     }
     
-    public boolean isEqual (Set <Temp> orig, Set <Temp> curr){
-    	// orig will always be small than curr
-    	for (Temp t : curr){
-    		if(!orig.contains(t)){
-    			orig.addAll(curr);	// add to orig Set
-    			return false;
-    		}
-    	}
-    	return true;
+	Set <Node> precolored = new LinkedHashSet <Node> ();
+	Set <Node> initial = new LinkedHashSet <Node> ();
+	Set <Node> simplifyWorklist = new LinkedHashSet <Node> (), freezeWorklist = new LinkedHashSet <Node> (),
+						spillWorklist = new LinkedHashSet <Node> (), spilledNodes = new LinkedHashSet <Node> (),
+						coalescedNodes = new LinkedHashSet <Node> (), coloredNodes = new LinkedHashSet <Node> ();
+	Stack <Node> selectStack = new Stack <Node> ();
+	Set <Move> worklistMoves = new LinkedHashSet <Move> ();
+	
+	public Hashtable <Temp, Node> temp2Node = new Hashtable <Temp, Node> ();
+	
+	public LinkedHashSet<Temp> arr2Set(Temp []u){
+    	LinkedHashSet<Temp> lh = new LinkedHashSet<Temp>();
+    	for (Temp ui : u)	//int i =0 ;i < u.length; i++)
+    		lh.add(ui);
+    	return lh;
     }
-
+    
+	private Node t2N(Temp t) {
+		Node n = temp2Node.get(t);
+		if (n == null) {
+			n = new Node(this,t);
+			temp2Node.put(t, n);
+		}
+		return n;
+	}
+	
     public Liveness(FlowGraph.AssemFlowGraph flow, Translate.Frame frame) {
         // TODO: Compute the interference graph representing liveness
         // information for the temporaris used and defined by the program represented
         // by flow.
     	
-    	// populate move list here by each instr in node n
-    	
-    	/*
-    	         for (AssemNode n : nodes()) {
-            LinkedList<Assem.Instr> instrs = n.instrs;
-            for (Assem.Instr i : instrs) {
-                for (Temp u : i.use)
-                    if (!n.def.contains(u))
-                        n.use.add(u);
-                for (Temp d : i.def)
-                    n.def.add(d);
-            }
-            Assem.Instr i = instrs.getLast();
-            for (Label l : i.jumps) {
-                AssemNode t = blocks.get(l);
-                if (t != null)
-                    addEdge(n, t);
-            }
-        }
-    	 */
+    	// populate move list here by each instr in node n TODO
     	
     	boolean repeat = false;
     	
@@ -88,6 +84,71 @@ public class Liveness extends InterferenceGraph {
     		
     	} while(repeat);
     	
+    //build();
+
+		for (AssemNode b : flow.nodes()) {
+			LinkedHashSet <Temp> live = new LinkedHashSet<Temp>(b.liveOut);
+			for (int i = b.instrs.size() - 1; i >= 0; i--) {
+				Instr inst = b.instrs.get(i);
+				
+				if (inst instanceof Instr.MOVE) {
+					live.removeAll(arr2Set(inst.use));
+					LinkedHashSet <Temp> nodes = arr2Set(inst.def);
+					nodes.addAll(arr2Set(inst.use));
+					for (Temp n : nodes)
+						t2N(n).moveList.add((Instr.MOVE)inst);		// where do we define these ?
+					moves.add((Instr.MOVE)inst);
+				}
+				
+				live.addAll(arr2Set(inst.def));
+				//)Addall(live, inst.def);	//live.addAll(inst.def);	CHECK is it pass by ref ?
+				
+				for (Temp d : inst.def){
+					Node n1 = new Node(this,d);
+					for (Temp l : live){
+						Node n2 = new Node(this,l);
+						addEdge(n1, n2);
+					}
+				}
+				live.removeAll(arr2Set(inst.def));
+				live.addAll(arr2Set(inst.use));
+			}
+		}
+	
+    }
+    
+    public void Addall (LinkedHashSet<Temp> l, Temp [] u){
+    	l.addAll(arr2Set(u));
+    }
+    
+
+    
+    public void build(){
+    	/*Build only adds an interference edge between a node that is defined
+    	at some point and the nodes that are currently live at that point. It is not necessary
+    	to add interferences between nodes in the live set. These edges will be added when
+    	processing other blocks in the program*/
+		/*for (BasicBlock b : blocks) {
+			HashSet <Temp> live = new HashSet<Temp>(b.live_out);
+			for (int i = b.list.size() - 1; i >= 0; i--) {
+				TExp inst = b.list.get(i);
+				if (inst instanceof Move) {
+					live.removeAll(inst.livenessNode.use);
+					HashSet <Temp> nodes = new HashSet<Temp>(inst.livenessNode.def);
+					nodes.addAll(inst.livenessNode.use);
+					for (Temp n : nodes)
+						t2N(n).moveList.add((Move)inst);
+					worklistMoves.add((Move)inst);
+				}
+				live.addAll(inst.livenessNode.def);
+				for (Temp d : inst.livenessNode.def)
+					for (Temp l : live)
+						addEdge(l, d);
+				live.removeAll(inst.livenessNode.def);
+				live.addAll(inst.livenessNode.use);
+			}
+		}
+	*/
     }
 
     public void show(java.io.PrintWriter out) {
